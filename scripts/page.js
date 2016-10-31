57f6a3235b65d7aed802302dad411d6d3439bf9b
BrainFooLong <brainfoolong@users.noreply.github.com>
2016-10-31 13:06:28 +0100

    updates frontpage

---
+++ scripts/page.js
@@ -0,0 +1,16 @@
+"use strict";
+$(function () {
+    var parallax = $(".parallax");
+    $(window).on("scroll", function () {
+        var st = $(document).scrollTop() + $(window).height() / 2;
+        parallax.each(function () {
+            var center = $(this).offset().top + 150;
+            var off = st - center;
+            off = 1 / 300 * off;
+            if (off > 1) off = 1;
+            if (off < -0.5) off = -0.5;
+            $(this).find("img").css("top", off * 100 + "px").css("transform", "scale("+(1.5 + (off))+")");
+
+        });
+    }).triggerHandler("scroll");
+});
